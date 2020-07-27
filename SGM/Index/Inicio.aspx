ad075012652647400944ce542ee139faaee70f3a
ErikCll <49504453+ErikCll@users.noreply.github.com>
2020-07-27 16:19:23 -0500

    Act

---
+++ SGM/Index/Inicio.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Inicio.aspx.cs" Inherits="Index.Inicio" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    Inicio
+    
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="mapeo" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="Content" runat="server">
+     <style>
+        .zoom {
+ 
+  transition: transform .2s; /* Animation */
+
+  margin: 0 auto;
+}
+
+.zoom:hover {
+  transform: scale(0.9); /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
+}
+    </style>
+        <div class="col-lg-12">
+        <br />
+        <div class="row">
+            <div class="col-sm-4 col-md-4 col-lg-4" runat="server" id="Panel1">
+                <div   class="form-group">
+
+                             <div class="card shadow zoom" style="border-left-color:#125886;border-left:solid steelblue">
+  <asp:LinkButton runat="server" ID="linkEtapa1" PostBackUrl="http://localhost:10931/Inicio.aspx" class="card-body  ">
+    <h4 class="card-title text-black">SGM</h4>
+    <p class="card-text text-black"></p>
+  </asp:LinkButton>
+</div>
+                </div>
+   
+            </div>
+                <div class="col-sm-4 col-md-4 col-lg-4">
+                    <div class="form-group">
+  
+
+                    </div>
+   
+            </div>
+                <div class="col-sm-4 col-md-4 col-lg-4">
+                    <div class="form-group">
+
+  
+                    </div>
+  
+            </div>
+        </div>
+    </div>
+
+</asp:Content>
---
+++ SGM/Index/Site1.Master.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Index {
+    
+    
+    public partial class Site1 {
+        
+        /// <summary>
+        /// Control lblInstalacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl lblInstalacion;
+        
+        /// <summary>
+        /// Control head.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        
+        /// <summary>
+        /// Control mapeo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder mapeo;
+        
+        /// <summary>
+        /// Control Content.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Content;
+    }
+}
